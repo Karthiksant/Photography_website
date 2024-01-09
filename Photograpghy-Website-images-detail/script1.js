cb79472fbd681fd37e94e7c9facb528494ed62ec
KARTHIK <135452522+Karthiksant@users.noreply.github.com>
2024-01-09 14:22:20 +0530

    Add files via upload

---
+++ Photograpghy-Website-images-detail/script1.js
@@ -0,0 +1,27 @@
+   function mainImage1( ){
+                       window.open("mainImage1.html");
+                    }
+                    function mainImage2( ){
+                       window.open("mainImage2.html");
+                    }
+                    function image1( ){
+                       window.open("image1.html");
+                    }
+                    function image2( ){
+                       window.open("image2.html");
+                    }
+                    function image3( ){
+                       window.open("image3.html");
+                    }
+                    function image4( ){
+                       window.open("image4.html");
+                    }
+                    function image5( ){
+                       window.open("image5.html");
+                    }
+                    function image6( ){
+                       window.open("image6.html");
+                    }
+                    function image7( ){
+                       window.open("image7.html");
+                    }
